50341c9e8f21a53b1f0ad5d8b3da8f039f9b2c2c
Maciej <Maciej@DESKTOP-LD4V0BD.home>
2019-05-22 00:29:18 +0200

    Edit Class Testowa

--- TestProject/src/test/Testowa.java
+++ TestProject/src/test/Testowa.java
@@ -1,7 +1,7 @@
 package test;
 
 public class Testowa {
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
